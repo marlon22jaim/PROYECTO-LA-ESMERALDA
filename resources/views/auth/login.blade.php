21f08f3c39ad0bce4bcf08ad0e20f8bc91211137
Marlon Jaimes <andres_jr19@hotmail.com>
2022-12-20 22:32:43 -0500

    Finalizando modificaciones en login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,7 +28,7 @@
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox"
                     class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <span class="ml-2 text-sm text-gray-600">{{ __('Recordarme') }}</span>
             </label>
         </div>
 
@@ -41,8 +41,8 @@
             </x-link>
 
         </div>
-        <x-primary-button class="ml-3">
-            {{ __('Log in') }}
+        <x-primary-button class="w-full justify-center">
+            {{ __('Iniciar Sesión') }}
         </x-primary-button>
     </form>
 </x-guest-layout>
