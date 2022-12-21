15e0b0ec41bf87438531174435ccc7585cfc1de1
Marlon Jaimes <andres_jr19@hotmail.com>
2022-12-20 22:50:05 -0500

    modificando la vista olvide mi contraseña y modificando los labels

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Olvidaste tu contraseña? No hay problema. solo pon tu email de registro y te enviaremos un enlace para que puedas restablecer tu contraseña.') }}
     </div>
 
     <!-- Session Status -->
@@ -12,13 +12,22 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
+        <div class="flex justify-between my-4">
+            <x-link :href="route('login')">
+                Iniciar Sesión
+            </x-link>
+            <x-link :href="route('register')">
+                Crear Cuenta
+            </x-link>
 
+        </div>
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Enviar Instrucciones') }}
             </x-primary-button>
         </div>
     </form>
