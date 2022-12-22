1f570e148dfa390a2f8b0b179313e3275073a673
Marlon Jaimes <andres_jr19@hotmail.com>
2022-12-21 20:24:22 -0500

    personalizando email de verificacion que se envia al usuario

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,5 +29,12 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject("Verificar tu cuenta, La Esmeralda")
+                ->line("Tu cuenta ya casi está lista, solo falta que presiones en el enlace de abajo y tu cuenta estara verificada y podras hacer uso de la plataforma")
+                ->action("Confirmar tu Cuenta", $url)
+                ->line("Si no has creado esta cuenta procede a ignorar este mensaje :)");
+        });
     }
 }
